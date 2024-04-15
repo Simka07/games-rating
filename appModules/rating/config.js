18128ee4c48081c4a172713a675c4f142d9b57ac
SimkaMari <masha@simonenko.ru>
2024-04-15 17:13:50 +0300

    project

---
+++ appModules/rating/config.js
@@ -0,0 +1,16 @@
+const PATH_TO_RATING_FILE = "./dataset/rating.json";
+
+
+const WEIGHT = {
+    gameplay: 2,
+    design: 1,
+    idea: 3,
+  }; 
+
+  module.exports = {
+    PATH_TO_RATING_FILE,
+    WEIGHT,
+    
+}
+
+  
